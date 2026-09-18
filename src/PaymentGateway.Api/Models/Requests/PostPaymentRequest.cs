a7e288913c6dac6c5ec50cc3338bd335f65998a5
Jerell James <jerell1998@gmail.com>
2026-09-19 00:15:08 +0100

    PostPaymentRequest validation

--- src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
+++ src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
@@ -1,11 +1,45 @@
-﻿namespace PaymentGateway.Api.Models.Requests;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class PostPaymentRequest
+namespace PaymentGateway.Api.Models.Requests;
+
+public class PostPaymentRequest : IValidatableObject
 {
-    public int CardNumberLastFour { get; set; }
-    public int ExpiryMonth { get; set; }
-    public int ExpiryYear { get; set; }
-    required public string Currency { get; set; }
-    public int Amount { get; set; }
-    public int Cvv { get; set; }
+    [Required, StringLength(19, MinimumLength = 14), RegularExpression(@"^\d+$")]
+    public required string CardNumber { get; set; }
+    [Range(1, 12)]
+    public required int ExpiryMonth { get; set; }
+    [Range(1, 9999)]
+    public required int ExpiryYear { get; set; }
+    [Required, StringLength(3, MinimumLength = 3)]
+    public required string Currency { get; set; }
+    [Range(1, int.MaxValue)]
+    public required int Amount { get; set; }
+    [Required, StringLength(4, MinimumLength = 3), RegularExpression(@"^\d+$")]
+    public required string Cvv { get; set; }
+
+    private static readonly HashSet<string> AllowedCurrencies =
+        new(StringComparer.Ordinal) { "GBP", "USD", "EUR" };
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext _)
+    {
+        if (!AllowedCurrencies.Contains(Currency)) {
+            yield return new ValidationResult("Unsupported currency.", [nameof(Currency)]);
+        }
+
+        // if the month is outside the range 1-12, this throws before the range validation so we need an early return
+        if (ExpiryMonth < 1 || ExpiryMonth > 12) yield break;
+        // similar for year
+        if (ExpiryYear < 1 || ExpiryYear > 9999) yield break;
+
+        //check expiry is in the future
+
+        // + 1 month - 1 tick because cards are valid until the end of the expiry month
+        var expiry = new DateTime(ExpiryYear, ExpiryMonth, 1).AddMonths(1).AddTicks(-1);
+        if (expiry <= DateTime.UtcNow) {
+            yield return new ValidationResult(
+                    "Expiry must be in the future.", 
+                    [nameof(ExpiryMonth), nameof(ExpiryYear)]
+                );
+        }
+    }
 }
