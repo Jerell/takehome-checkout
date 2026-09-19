ebbc18c2ba8613c96950fdba8ea7a5d03ed21726
Jerell James <jerell1998@gmail.com>
2026-09-19 14:00:59 +0100

    change CardNumberLastFour to string to preserve leading zeros

--- src/PaymentGateway.Api/Models/Responses/PostPaymentResponse.cs
+++ src/PaymentGateway.Api/Models/Responses/PostPaymentResponse.cs
@@ -4,7 +4,7 @@ public class PostPaymentResponse
 {
     public Guid Id { get; set; }
     public PaymentStatus Status { get; set; }
-    public int CardNumberLastFour { get; set; }
+    public required string CardNumberLastFour { get; set; }
     public int ExpiryMonth { get; set; }
     public int ExpiryYear { get; set; }
     public required string Currency { get; set; }
